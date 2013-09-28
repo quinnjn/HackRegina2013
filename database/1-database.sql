42bb9f9905bc3ab590bf856383072b12e66905fd
Alexa <alexahlelliott@gmail.com>
2013-09-28 13:57:24 -0600

    fixed characters in facilities, changes to table creation, added data inserting file

--- database/1-database.sql
+++ database/1-database.sql
@@ -4,28 +4,27 @@ CREATE TABLE HACKATHON.facilities(
 	rha				int,
 	rha_name		varchar(30),
 	facility_id		varchar(15),
-	facility_name	varchar(65) NOT NULL,
+	facility_name	varchar(65),
 	facility_type	varchar(40),
 	type_id			int,
 	hospital_designation varchar(40),
 	street_address	varchar(35),
 	location		varchar(25),
 	postal_code		varchar(7),
-	lat_dd			float(10, 7),
-	long_dd			float(10, 7),
+	lat_dd			float(13, 9),
+	long_dd			float(13, 9),
 	PRIMARY KEY(facility_name)
 );
 CREATE TABLE HACKATHON.doctors(
 	provider_id			int,
-	group_number		int,
+	group_number		varchar(15),
 	num_phys_in_group	int,
 	clinic_name			varchar(40),
 	community			varchar(25),
 	postal_code			varchar(7),
 	doctor_name			varchar(35),
 	specialty			varchar(55),
-	rha_name			varchar(30),
-	PRIMARY KEY(doctor_name, clinic_name)
+	rha_name			varchar(30)
 );
 
 
