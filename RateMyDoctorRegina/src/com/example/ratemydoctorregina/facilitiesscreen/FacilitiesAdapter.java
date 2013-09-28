c97d8aefb51c45d86a2020e84ab677ca5d67d961
Jordan Johns <JordanJohns.c@gmail.com>
2013-09-28 16:23:57 -0600

    - Sorting for facilities works now.  Filtering left out.

--- RateMyDoctorRegina/src/com/example/ratemydoctorregina/facilitiesscreen/FacilitiesAdapter.java
+++ RateMyDoctorRegina/src/com/example/ratemydoctorregina/facilitiesscreen/FacilitiesAdapter.java
@@ -1,10 +1,13 @@
 package com.example.ratemydoctorregina.facilitiesscreen;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import com.example.ratemydoctorregina.R;
 import com.example.ratemydoctorregina.model.Doctor;
 import com.example.ratemydoctorregina.model.Facility;
+import com.example.ratemydoctorregina.physiciansscreen.PhysiciansAdapter.SortBy;
 
 import android.app.Activity;
 import android.view.View;
@@ -15,9 +18,16 @@ import android.widget.TextView;
 
 public class FacilitiesAdapter extends BaseAdapter 
 {
-
+	public enum SortBy { NAME, BEST, WORST }
+	
+	ArrayList<Facility> _master;
 	ArrayList<Facility> _facilities;
+	
 	Activity _context;
+	
+	ArrayList<Facility> _specialty_sorted;
+	ArrayList<Facility> _best_sorted;
+	ArrayList<Facility> _worst_sorted;
 		
 	public FacilitiesAdapter(Activity context)
 	{
@@ -28,6 +38,14 @@ public class FacilitiesAdapter extends BaseAdapter
 	public void setData(ArrayList<Facility> facilities)
 	{
 		_facilities = facilities;
+		
+		_master = new ArrayList<Facility>();
+		_master.addAll(_facilities);
+		
+		_specialty_sorted = null;
+		_best_sorted = null;
+		_worst_sorted = null;
+		
 		notifyDataSetChanged();
 	}
 	
@@ -70,6 +88,52 @@ public class FacilitiesAdapter extends BaseAdapter
 		((TextView)view.findViewById(R.id.rating_txt)).setText(String.valueOf(value));
 		
 		return view;
+	}
+	
+	public void sortBy(SortBy type)
+	{
+		if(type == SortBy.BEST)
+		{
+			if(_best_sorted == null)
+			{
+				Collections.sort(_facilities, new Comparator<Facility>() 
+						{
+							@Override
+							public int compare(Facility lhs, Facility rhs) 
+							{
+								return (int)((lhs.get_rating() - rhs.get_rating()) * 100);
+							}
+						});
+				_best_sorted = new ArrayList<Facility>();
+				_best_sorted.addAll(_facilities);
+			}
+			
+			_facilities = _best_sorted;			
+		}
+		else if(type == SortBy.WORST)
+		{
+			if(_worst_sorted == null)
+			{
+				Collections.sort(_facilities, new Comparator<Facility>() 
+						{
+							@Override
+							public int compare(Facility lhs, Facility rhs) 
+							{
+								return (int)((rhs.get_rating() - lhs.get_rating()) * 100);
+							}
+						});
+				_worst_sorted = new ArrayList<Facility>();
+				_worst_sorted.addAll(_facilities);
+			}
+			
+			_facilities = _worst_sorted;
+		}
+		else
+		{
+			_facilities = _master;
+		}
+		
+		notifyDataSetChanged();
 	}
 
 }
