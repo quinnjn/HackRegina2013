ab87e1d52ed9f8beaf2cff9af349c792e2e53e78
Alexa <alexahlelliott@gmail.com>
2013-09-28 11:50:18 -0600

    adding pydev project and database creation file

---
+++ database/1-database.sql
@@ -0,0 +1,21 @@
+DROP SCHEMA HACKATHON;
+CREATE SCHEMA HACKATHON;
+CREATE TABLE HACKATHON.Facilities(
+	table_id 		int NOT NULL,
+	rha				int,
+	rha_name		varchar(30),
+	facility_id		varchar(15),
+	facility_name	varchar(65),
+	facility_type	varchar(40),
+	type_id			int,
+	hospital_designation varchar(40),
+	street_address	varchar(35),
+	location		varchar(20),
+	postal_code		varchar(7),
+	lat_dd			float(10, 7),
+	long_dd			float(10, 7),
+	PRIMARY KEY(table_id)
+)
+
+
+
